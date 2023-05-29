14b6e204adb22e78e07536cf6d8700cb500d834f
Abhishek P <abhikash01@gmail.com>
2023-05-29 14:10:50 +0530

    Add project files.

---
+++ APIGateway/Program.cs
@@ -0,0 +1,34 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddRazorPages();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+await app.UseOcelot();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+
+app.Run();
---
+++ CatelogService/Controllers/CatalogController.cs
@@ -0,0 +1,23 @@
+﻿using CatelogService.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatelogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        DatabaseContext _db;
+        public CatalogController(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _db.Products;
+        }
+    }
+}
